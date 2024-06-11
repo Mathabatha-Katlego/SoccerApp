26ee737e0582135cc7f9310719929fe67a2cdcda
Mathabatha Katlego <mathabathamohlaloga@gmail.com>
2024-06-11 21:00:38 +0200

    third commit

---
+++ 14-Exceptions/Practices/Practice1/14-Exceptions_Practice1/src/utility/PlayerDatabaseException.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utility;
+
+/**
+ *
+ * @author Mathabatha
+ */
+public class PlayerDatabaseException extends Exception {
+
+    public PlayerDatabaseException(String message) {
+        super(message);
+    }
+
+}
